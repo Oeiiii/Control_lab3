dc1d0952c80e99121e400c36258d21e73a6a4c55
Oeiiii <worranittha.larp@mail.kmutt.ac.th>
2022-04-24 16:03:34 +0700

    edit period

--- LAB3/Core/Src/main.c
+++ LAB3/Core/Src/main.c
@@ -390,7 +390,7 @@ static void MX_TIM11_Init(void)
   htim11.Instance = TIM11;
   htim11.Init.Prescaler = 99;
   htim11.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim11.Init.Period = 65535;
+  htim11.Init.Period = 999;
   htim11.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim11.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim11) != HAL_OK)
